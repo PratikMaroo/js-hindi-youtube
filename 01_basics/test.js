0cb309425d0c42899e0cd51b1b05d44ff2df33ef
PratikMaroo <104310498+PratikMaroo@users.noreply.github.com>
2024-03-14 14:32:41 +0000

    for in

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,25 @@
 console.log("Pratik")
+//to print
+
+const arr = [2,4,6,8]
+let element = ""
+for (let index = 0; index < arr.length; index++) {
+     element += arr[index] + ' '
+    if(arr[index] == 6)
+    break
+}
+//console.log(element)
+//console.log(typeof(element));
+ 
+// const arr1 = [2,4,6,8]
+
+// for (const key of arr1) {
+        // console.log(key);
+// }
+
+// const str  = 'hello'
+
+// for (const key in str)  {
+//    console.log(str[key]);
+// }
+
