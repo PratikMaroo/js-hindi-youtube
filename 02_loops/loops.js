2ba5f23f1bbb3745fcf9d2a910f184bdf39b4b54
PratikMaroo <104310498+PratikMaroo@users.noreply.github.com>
2024-03-20 10:26:33 +0000

    Iterations

---
+++ 02_loops/loops.js
@@ -0,0 +1,20 @@
+let arr = [0,2,4,6,8]
+
+for (let i = 0; i < arr.length; i++) {
+    //console.log(arr[i])
+}
+
+const arr1 = ["naruto","sasuke","sakura"]
+
+for (let index = 0; index < arr1.length; index++) {
+    const element = arr1[index];
+   // console.log(element);
+}
+
+
+for(let i =0;i<10;i++){
+
+    for(let j=0;j<10;j++){
+       // console.log(`for every value of j ${j} i is ${i}`);
+    }
+}
